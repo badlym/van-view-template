714d3b60fef735d2898b562296c3359158332d92
liuchengjin <75836368@qq.com>
2023-05-25 11:23:00 +0800

    feat: 删除无用代码

--- build/constant.ts
+++ build/constant.ts
@@ -2,4 +2,4 @@
  * The name of the configuration file entered in the production environment
  */
 export const GLOB_CONFIG_FILE_NAME = '_app.config.js';
-export const OUTPUT_DIR = 'E10077531';
+export const OUTPUT_DIR = 'dist';
